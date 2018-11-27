85069a0e2961bae5950f9b34342dfe79d463e94d
The Incognito Coder <theincognitocoder@gmail.com>
2018-11-26 21:00:26 -0800

    Update specs to use rspec matcher method.

--- spec/rspec/json_helpers/equal_json_matcher_spec.rb
+++ spec/rspec/json_helpers/equal_json_matcher_spec.rb
@@ -5,13 +5,11 @@ module RSpec
 
     describe EqualJsonMatcher do
       it 'returns true when JSON values are equal' do
-        matcher = EqualJsonMatcher.new('{}')
-        expect(matcher.matches?('{}')).to be(true)
+        expect('{}').to equal_json('{}')
       end
 
       it 'returns false when JSON values are not equal' do
-        matcher = EqualJsonMatcher.new('{}')
-        expect(matcher.matches?('[]')).to be(false)
+        expect('{}').not_to equal_json('[]')
       end
 
       it 'it compares JSON documents indifferently of whitespace' do
@@ -21,50 +19,45 @@ module RSpec
             "foo" : "bar"
           }
         JSON
-        matcher = EqualJsonMatcher.new(expected)
-        expect(matcher.matches?(actual)).to be(true)
+        expect(actual).to equal_json(expected)
       end
 
       it 'it compares JSON documents indifferently of attribute order' do
         expected = '{"abc":"abc-value", "xyz": "xyz-value"}'
         actual = '{"xyz":"xyz-value", "abc": "abc-value"}'
-        matcher = EqualJsonMatcher.new(expected)
-        expect(matcher.matches?(actual)).to be(true)
+        expect(actual).to equal_json(expected)
       end
 
       it 'expects actual values to not have extra attributes' do
         expected = '{"abc": "abc-value"}'
         actual = '{"abc":"abc-value", "xyz": "xyz-value"}'
-        matcher = EqualJsonMatcher.new(expected)
-        expect(matcher.matches?(actual)).to be(false)
+        expect(actual).not_to equal_json(expected)
       end
 
       it 'expects actual values to not have fewer attributes' do
         expected = '{"abc":"abc-value", "xyz": "xyz-value"}'
         actual = '{"abc": "abc-value"}'
-        matcher = EqualJsonMatcher.new(expected)
-        expect(matcher.matches?(actual)).to be(false)
+        expect(actual).not_to equal_json(expected)
       end
 
       it 'compares JSON arrays' do
         expected = '{ "items": [1,2,3] }'
         actual = '{ "items": [1,2,3] }'
-        matcher = EqualJsonMatcher.new(expected)
-        expect(matcher.matches?(actual)).to be(true)
+        expect(actual).to equal_json(expected)
       end
 
       it 'expects array values to have the same order' do
         expected = '[1,2,3]'
         actual = '[3,2,1]'
-        matcher = EqualJsonMatcher.new(expected)
-        expect(matcher.matches?(actual)).to be(false)
+        expect(actual).not_to equal_json(expected)
       end
 
       it 'provides a helpful diff when the JSON values do not match' do
         expected = '{ "items": [1,2,3] }'
         actual = '{ "items": [3,2,1] }'
+        # using matcher class to disable colorization
         matcher = EqualJsonMatcher.new(expected, colorize: false)
-        matcher.matches?(actual) # false
+        expect(matcher.matches?(actual)).to be(false)
         expect(matcher.failure_message).to eq(<<~MESSAGE)
           expected: {
             "items": [
@@ -95,21 +88,21 @@ module RSpec
       end
 
       it 'provides a helpful failure message when actual is the wrong type' do
-        matcher = EqualJsonMatcher.new('{}')
-        matcher.matches?(nil)
-        expect(matcher.failure_message).to eq('Expected a JSON string, got NilClass.')
+        expect do
+          expect(nil).to equal_json('{}')
+        end.to raise_error('Expected a JSON string, got NilClass.')
       end
 
       it 'raises an error when the expected value is not a string' do
         expect do
-          EqualJsonMatcher.new(123)
-        end.to raise_error(ArgumentError, /Expected a JSON string, got (Fixnum|Integer)./)
+          expect('{}').to equal_json(nil)
+        end.to raise_error('Expected a JSON string, got NilClass.')
       end
 
       it 'raises an error when the expected value is not valid JSON' do
         expect do
-          EqualJsonMatcher.new('---')
-        end.to raise_error(ArgumentError, /Expected a string containing valid JSON/)
+          expect('---').to equal_json('{}')
+        end.to raise_error(/Expected a string containing valid JSON/)
       end
     end
 
